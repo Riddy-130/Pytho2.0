e3d1e55905af01b86893a30b27af04614628f5e7
Riddy-130 <riddy22205101830@diu.edu.bd>
2024-09-18 20:15:50 +0600

    swap

---
+++ OOP-II/Lab report -I/swap.py
@@ -0,0 +1,7 @@
+x = input("enter the value of x : ")
+y = input("enter the value of y : ")
+temp = x
+x = y
+y = temp
+print("value of x after swap:{} ".format(x))
+print("value of y after swap:{} ".format(y))
