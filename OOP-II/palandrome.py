5b0ed56a6d368e02d897c30d0c904da0ae9509c6
Riddy-130 <riddy22205101830@diu.edu.bd>
2024-09-25 22:37:01 +0600

    palandrome

---
+++ OOP-II/palandrome.py
@@ -0,0 +1,11 @@
+def is_palindrome(s):
+    
+    s = s.lower()
+    
+    return s == s[::-1]
+
+input_string = "Madam"
+if is_palindrome(input_string):
+    print(f'"{input_string}" is a palindrome.')
+else:
+    print(f'"{input_string}" is not a palindrome.')
